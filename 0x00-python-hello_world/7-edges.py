af584c6b90bcdabbb79ad2d50b5012332bda345b
Olalekan Okewale <olalekanokewale2020@gmail.com>
2022-08-24 12:09:10 +0000

    hello world

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+word = "Holberton"
+print(f"First 3 letters: ", end="")
+print(word[0:3])
+print(f"Last 2 letters: ", end="")
+print(word[7:9])
+print(f"Middle word: ", end="")
+print(word[1:8])
