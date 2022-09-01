20b07029639a4d05a2163f56d35bd23b09af043c
Olalekan Okewale <olalekanokewale2020@gmail.com>
2022-09-01 05:00:13 +0000

    pushed

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is not None:
-        max_val = max(a_dictionary.keys(), key=(lambda x: a_dictionary[x]))
-        return (a_dictionary[max_val])
-    return None
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
+    item = list(a_dictionary.keys())[0]
+    big = a_dictionary[item]
+    for key, value in a_dictionary.items():
+        if value > big:
+            big = value
+            item = key
+    return (item)
--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    for i in range(len(a_dictionary)):
-        keys = i + 1
-    return (keys)
+    if a_dictionary is not None:
+        for i in range(len(a_dictionary)):
+            keys = i + 1
+        return (keys)
+    return (0)
