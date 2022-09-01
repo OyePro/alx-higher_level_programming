94b7420697c769799bf957902fb697c4c22b0748
Olalekan Okewale <olalekanokewale2020@gmail.com>
2022-09-01 05:35:43 +0000

    no of keys

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
     if a_dictionary is not None:
-        for i in range(len(a_dictionary)):
-            keys = i + 1
-        return (keys)
+        return(len(a_dictionary.keys()))
     return (0)
