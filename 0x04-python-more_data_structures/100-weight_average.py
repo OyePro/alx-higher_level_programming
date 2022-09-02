67e673a60f6d7448e2d51af7970e42bb206be950
Olalekan Okewale <olalekanokewale2020@gmail.com>
2022-09-02 21:03:08 +0000

    advanced

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
+    if not isinstance(my_list, list) or my_list == "":
+        return (0)
+    mul = 0
+    div = 0
+    for i in my_list:
+        mul += i[0] * i[1]
+        div += i[1]
+        average = mul / div
+    return (average)
