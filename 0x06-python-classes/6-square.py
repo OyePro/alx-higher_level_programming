c30191cba9eb63ab18753615c480dbd43d338ac1
Olalekan Okewale <olalekanokewale2020@gmail.com>
2022-09-14 02:57:45 +0000

    classes and objects

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -70,9 +70,7 @@ class Square:
 
         if self.__size == 0:
             print()
-            return
-        for i in range(self.__position[1]):
-            print()
-        for j in range(self.__size):
-            print("".join([" " for k in range(self.__position[0])]), end="")
-            print("".join(["#" for l in range(self.__size)]))
+        else:
+            print("\n" * self.__position[1], end="")
+            print("\n".join([" " * self.__position[0] + "#" * self.__size
+                             for rows in range(self.__size)]))
