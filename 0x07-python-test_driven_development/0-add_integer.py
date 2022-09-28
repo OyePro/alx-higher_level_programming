693e6fab9b414396d349e33525dccc840535fea4
Olalekan Okewale <olalekanokewale2020@gmail.com>
2022-09-28 08:22:20 +0100

    testing

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
+"""defining function add_integer to add a+b"""
+
 
 def add_integer(a, b=98):
-    """Function to add two integers
-    Args:
-        a - first integer
-        b - second integer
-    Return:
-        a + b
+    """
+    Return: a + b as int
     """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
