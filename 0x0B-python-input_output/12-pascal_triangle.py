8faaa7c4128a08f9be88fd67ead25141d674ef4d
Olalekan Okewale <olalekanokewale2020@gmail.com>
2022-10-03 08:27:14 +0100

    input and output

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -7,12 +7,12 @@ def pascal_triangle(n):
     returns a list of lists of integers
     """
     if n <= 0:
-        return [[]]
+        return []
     tri = [[1]]
     for i in range(n - 1):
-        row = [1, 1]
+        row = [1]
         for j in range(i):
-            row = []
-            row.append(",".join(map(str, str(11**(i + 1)))))
+            row.append(tri[i][j] + tri[i][j + 1])
+        row.append(1)
         tri.append(row)
     return (tri)
