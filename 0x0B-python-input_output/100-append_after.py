5493b313f67bbf776a6c5794843451356242cf5b
Olalekan Okewale <olalekanokewale2020@gmail.com>
2022-10-03 10:56:00 +0100

    append after

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -8,9 +8,10 @@ def append_after(filename="", search_string="", new_string=""):
     a specific string
     """
     with open(filename, mode='r+', encoding='utf-8') as f:
-        contents = f.readlines()
-        for index, line in enumerate(contents):
+        contents = ""
+        for line in f:
+            contents += line
             if search_string in line:
-                contents.insert((index + 1), new_string)
+                contents += new_string        
         f.seek(0)
         f.writelines(contents)
