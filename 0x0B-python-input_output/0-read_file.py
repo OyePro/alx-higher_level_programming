214501bc2de1b865fe4dcb4a033f0de6d86bdcc6
Olalekan Okewale <olalekanokewale2020@gmail.com>
2022-10-03 07:48:49 +0100

    input and output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """method to read a textfile and print to standard output"""
     with open(filename, encoding="utf-8") as f:
         read_name = f.read()
-    print(read_name)
+    print(read_name, end="")
--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+A script that adds all arguments to a Python list,
+and then save them to a file
+"""
+
+
 if __name__ == "__main__":
     from sys import argv
     save_json_file = __import__('5-save_to_json_file').save_to_json_file
