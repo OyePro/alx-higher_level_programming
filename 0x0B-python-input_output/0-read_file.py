acace118561027ee52b0ed41a5ef7aeaf004b99b
Olalekan Okewale <104862865+OyePro@users.noreply.github.com>
2023-08-30 21:56:14 +0100

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/0-read_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Defining function read_file"""
-
-
-def read_file(filename=""):
-    """method to read a textfile and print to standard output"""
-    with open(filename, encoding="utf-8") as f:
-        read_name = f.read()
-    print(read_name, end="")
--- 0x0B-python-input_output/8-class_to_json.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Defining function class_to_json"""
-
-
-def class_to_json(obj):
-    """
-    returns dictionary descriptions with simple data structure
-    """
-    return (obj.__dict__)
