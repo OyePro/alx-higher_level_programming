c8e865610ff2cf4619309d556f23093254835631
Olalekan Okewale <olalekanokewale2020@gmail.com>
2023-11-16 00:58:37 +0100

    objects and scopes

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
 
 exports.nbOccurences = function (list, searchElement) {
-    let count = 0;
-    for (let i in list) {
-      if (list[i] === searchElement) {
-        count++;
-      }
+  let count = 0;
+  for (const i in list) {
+    if (list[i] === searchElement) {
+      count++;
     }
-    return count;
-  };
-  
+  }
+  return count;
+};
