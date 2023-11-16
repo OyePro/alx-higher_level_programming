8694b7f93beeebcf428b7ba8e6327740141431f8
Olalekan Okewale <olalekanokewale2020@gmail.com>
2023-11-16 09:34:36 +0100

    up

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 
 const args = process.argv;
-if (args.length <= 2) {
-  console.log('Missing number of occurrences');
-} else if (isNaN(args[2])) {
+if (args.length <= 2 || isNaN(args[2])) {
   console.log('Missing number of occurrences');
 } else {
   for (let i = 0; i < +args[2]; i++) {
