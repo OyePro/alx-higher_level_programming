83e724d434f36cad20cdef94b74c6281a23b4cae
Olalekan Okewale <olalekanokewale2020@gmail.com>
2023-11-16 10:26:52 +0100

    second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,11 +5,11 @@ function secondBig (arr) {
   let first = -1;
   let second = -1;
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > first) {
+    if (+arr[i] > first) {
       second = first;
-      first = arr[i];
-    } else if (arr[i] > second && arr[i] !== first) {
-      second = arr[i];
+      first = +arr[i];
+    } else if (+arr[i] > second && +arr[i] !== first) {
+      second = +arr[i];
     }
   }
   return (second);
