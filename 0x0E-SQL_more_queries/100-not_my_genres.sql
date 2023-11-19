44c27762b4c3ec21106bbc7dfd5675f9818238aa
Olalekan Okewale <olalekanokewale2020@gmail.com>
2023-11-19 07:50:57 +0100

    dexter

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -10,5 +10,5 @@
             ON tv_genres.id = tv_show_genres.genre_id
      INNER JOIN tv_shows
 	    ON tv_shows.id = tv_show_genres.show_id
-	 WHERE tv_shows.id = 8)
+	 WHERE tv_shows.title = "Dexter")
       ORDER BY name ASC;
